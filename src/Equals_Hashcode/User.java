eb6f6b7c3fe7dd8210a43fc10fb7b354137cb546
CCHQSA <mykolalototskyig@gmail.com>
2026-08-22 20:46:12 +0300

    Equals and Hashcode

---
+++ src/Equals_Hashcode/User.java
@@ -0,0 +1,159 @@
+package Equals_Hashcode;
+
+import java.util.Objects;
+
+public class User {
+    Long id;
+    String username;
+    String password;
+    String email;
+    String name;
+    String surname;
+    String phone;
+    String address;
+    String city;
+
+    public User(
+            Long id, String username, String password,
+                String email, String name, String surname,
+                String phone, String address, String city
+    ) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getPassword(), user.getPassword()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getName(), user.getName()) && Objects.equals(getSurname(), user.getSurname()) && Objects.equals(getPhone(), user.getPhone()) && Objects.equals(getAddress(), user.getAddress()) && Objects.equals(getCity(), user.getCity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getPassword(), getEmail(), getName(), getSurname(), getPhone(), getAddress(), getCity());
+    }
+
+    static void main() {
+        User user = new User(
+                1L,
+                "johndoe",
+                "p@ssword123",
+                "john.doe@example.com",
+                "John",
+                "Doe",
+                "+380501234567",
+                "Khreshchatyk St, 1",
+                "Kyiv"
+        );
+
+        User user2 = new User(
+                2L,
+                "johndoe",
+                "p@ssword123",
+                "john.doe@example.com",
+                "John",
+                "Doe",
+                "+380501234567",
+                "Khreshchatyk St, 1",
+                "Kyiv"
+        );
+
+        System.out.println(user.equals(user2));
+
+    }
+}
