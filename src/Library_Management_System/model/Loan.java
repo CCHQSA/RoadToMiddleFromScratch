41531eeb8a8dd1d9f6d30553420e5b43e5ee3228
CCHQSA <mykolalototskyig@gmail.com>
2026-09-17 23:41:27 +0300

    Make some Loan fields null-safety checks in constructor

--- src/Library_Management_System/model/Loan.java
+++ src/Library_Management_System/model/Loan.java
@@ -1,23 +1,52 @@
 package Library_Management_System.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Loan {
-    private final long id;
-    private Book book;
-    private User user;
-    private LocalDate borrowDate;
-    private LocalDate dueDate;
-    private LocalDate returnDate;
-    private LoanStatus status;
+    private final Long id;
+    private final Book book;
+    private final User user;
+    private final LocalDate borrowDate;
+    private final LocalDate dueDate;
+    private final LocalDate returnDate;
+    private final LoanStatus status;
 
-    public Loan(User user,Book book, long id){
-        this.user = user;
-        this.id = id;
-        this.book = book;
+    public Loan(Long id, Book book, User user, LocalDate dueDate) {
+        this.id = Objects.requireNonNull(id, "ID cannot be null");
+        this.book = Objects.requireNonNull(book, "Book cannot be null");
+        this.user = Objects.requireNonNull(user, "User cannot be null");
         this.borrowDate = LocalDate.now();
-        this.dueDate = this.borrowDate.plusDays(14);
+        this.dueDate = borrowDate.plusDays(14);
+        this.returnDate = null;
         this.status = LoanStatus.ACTIVE;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public LoanStatus getStatus() {
+        return status;
+    }
 }
