6cdf2bcf6747159a70c06d23c31f442b0b3b44a2
CCHQSA <mykolalototskyig@gmail.com>
2026-09-19 14:37:35 +0300

    test: clean up AuthorServiceTest

--- test/Library_Management_System/service/AuthorServiceTest.java
+++ test/Library_Management_System/service/AuthorServiceTest.java
@@ -3,7 +3,6 @@ package Library_Management_System.service;
 import Library_Management_System.exception.AuthorNotFoundException;
 import Library_Management_System.model.Author;
 import Library_Management_System.model.Library;
-import Library_Management_System.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,58 +22,56 @@ class AuthorServiceTest {
     void setUp() {
         library = new Library();
 
-        authorA = new Author(1L, "Jonathan", "Joestar", LocalDate.of(1890,  3, 4));
-        authorB = new Author(2L, "Jotaro", "Joestar", LocalDate.of(1940,  5, 13));
+        authorA = new Author(1L, "Jonathan", "Joestar", LocalDate.of(1890, 3, 4));
+        authorB = new Author(2L, "Jotaro", "Joestar", LocalDate.of(1940, 5, 13));
 
         library.addAuthor(authorA);
         library.addAuthor(authorB);
-
 
         authorService = new AuthorService(library);
     }
 
     @Test
-    void addAuthor() {
-        Author authorC = new Author(3L, "Hisoka", "Morro", LocalDate.of(1940,  5, 13));
-        Author authorD = new Author(4L, "Test", "NoAdd", LocalDate.of(1940,  5, 13));
+    void addAuthor_ShouldSuccessfullyAddAuthor() {
+        Author authorC = new Author(3L, "Hisoka", "Morro", LocalDate.of(1940, 5, 13));
 
         authorService.addAuthor(authorC);
 
-        assertNotNull(library.getAuthors(), "Authors list should not be null");
-        assertEquals(3, library.getAuthors().size(), "Authors list should contain 3");
-        assertTrue(library.getAuthors().containsValue(authorC), "Authors list should contain authorC");
-        assertTrue(library.getAuthors().containsValue(authorB), "Authors list should contain authorB");
-        assertTrue(library.getAuthors().containsValue(authorA), "Authors list should contain authorA");
+        assertEquals(authorC, authorService.findAuthorById(3L), "Should find the newly added author");
     }
 
     @Test
-    void findAuthorById() {
-        Author authorC = new Author(3L, "Test", "NoAdd", LocalDate.of(1940,  5, 13));
-
+    void findAuthorById_ShouldReturnAuthor_WhenIdExists() {
         assertEquals(authorA, authorService.findAuthorById(1L));
         assertEquals(authorB, authorService.findAuthorById(2L));
-        assertThrows(AuthorNotFoundException.class, () -> authorService.findAuthorById(3L));
-
     }
 
     @Test
-    void removeAuthor() {
+    void findAuthorById_ShouldThrowException_WhenIdDoesNotExist() {
+        assertThrows(AuthorNotFoundException.class, () -> authorService.findAuthorById(999L));
+    }
+
+    @Test
+    void removeAuthor_ShouldDeleteAuthorFromLibrary() {
         authorService.removeAuthor(1L);
-        assertNotNull(library.getAuthors(), "Authors list should NOT be null");
-        assertFalse(library.getAuthors().containsValue(authorA), "Authors list should contain authorA");
-        assertTrue(library.getAuthors().containsValue(authorB), "Authors list should contain authorB");
+
+        assertThrows(AuthorNotFoundException.class, () -> authorService.findAuthorById(1L),
+                "Should throw exception because author was removed");
     }
 
     @Test
-    void findAuthorsByName() {
+    void findAuthorsByName_ShouldReturnCorrectAuthors() {
         List<Author> foundByFirstName = authorService.findAuthorsByName("Jonathan");
         List<Author> foundByLastName = authorService.findAuthorsByName("Joestar");
-        List<Author> foundByFullName = authorService.findAuthorsByName("Jonathan Joestar");
-        List<Author> notFound = authorService.findAuthorsByName("Name");
+        List<Author> notFound = authorService.findAuthorsByName("NonExistingName");
 
-        assertNotNull(foundByFirstName, "foundByFirstName should NOT be null");
-        assertNotNull(foundByLastName, "foundByLastName should NOT be null");
-        assertNotNull(foundByFullName, "foundByFullName should NOT be null");
-        assertTrue(notFound.isEmpty(), "notFound should be empty");
+        assertEquals(1, foundByFirstName.size());
+        assertTrue(foundByFirstName.contains(authorA));
+
+        assertEquals(2, foundByLastName.size());
+        assertTrue(foundByLastName.contains(authorA));
+        assertTrue(foundByLastName.contains(authorB));
+
+        assertTrue(notFound.isEmpty(), "Should return an empty list if no match found");
     }
 }
